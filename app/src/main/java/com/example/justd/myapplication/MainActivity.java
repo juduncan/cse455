558ff2b508a27571775861f7f1477ae6917067ff
jamesdileva <jamesdileva@gmail.com>
2017-10-11 01:40:38 -0700

    Added two buttons a testmap and setting button, when I pulled the setting button wasn't there but Diego's work was which was the Google login page.

--- app/src/main/java/com/example/justd/myapplication/MainActivity.java
+++ app/src/main/java/com/example/justd/myapplication/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.justd.myapplication;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
@@ -22,8 +25,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         SignIn = (SignInButton)findViewById(R.id.bn_login);
         SignIn.setOnClickListener(this);
 
+        if(googleServicesAvail()){
+            Toast.makeText(this,"You have Google Play Services YAY", Toast.LENGTH_LONG).show();
+
+        }
     }
 
+
+    public boolean googleServicesAvail(){
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if(isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)){
+            Dialog msg =  api.getErrorDialog(this,isAvailable,0);
+            msg.show();
+        } else {
+            Toast.makeText(this, "Can't connect to play services :*(", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
     /** Called when the user taps the login button **/
     public void goMap(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
