6bf643fb5ac668ed88516afa3a8171adcb546de2
Diego Quinonez <quinonezdiego@yahoo.com>
2017-10-10 19:37:05 -0700

    update

--- app/src/main/java/com/example/justd/myapplication/MainActivity.java
+++ app/src/main/java/com/example/justd/myapplication/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.justd.myapplication;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener  {
+
+    private SignInButton SignIn;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_CODE = 9001;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SignIn = (SignInButton)findViewById(R.id.bn_login);
+        SignIn.setOnClickListener(this);
+
     }
 
     /** Called when the user taps the login button **/
@@ -25,5 +36,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
 //
