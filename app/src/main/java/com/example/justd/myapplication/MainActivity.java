ac879c5cbf1d5cdc02152497a5e39267c0378f0b
Diego Quinonez <quinonezdiego@yahoo.com>
2017-10-14 13:24:29 -0700

    Sign in

--- app/src/main/java/com/example/justd/myapplication/MainActivity.java
+++ app/src/main/java/com/example/justd/myapplication/MainActivity.java
@@ -8,44 +8,55 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener  {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,GoogleApiClient.OnConnectionFailedListener {
 
     private SignInButton SignIn;
     private GoogleApiClient googleApiClient;
     private static final int REQ_CODE = 9001;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SignIn = (SignInButton)findViewById(R.id.bn_login);
+        SignIn = (SignInButton) findViewById(R.id.bn_login);
         SignIn.setOnClickListener(this);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
 
-        if(googleServicesAvail()){
-            Toast.makeText(this,"You have Google Play Services YAY", Toast.LENGTH_LONG).show();
+
+        if (googleServicesAvail()) {
+            Toast.makeText(this, "You have Google Play Services YAY", Toast.LENGTH_LONG).show();
 
         }
     }
 
 
-    public boolean googleServicesAvail(){
+    public boolean googleServicesAvail() {
         GoogleApiAvailability api = GoogleApiAvailability.getInstance();
         int isAvailable = api.isGooglePlayServicesAvailable(this);
-        if(isAvailable == ConnectionResult.SUCCESS) {
+        if (isAvailable == ConnectionResult.SUCCESS) {
             return true;
-        } else if (api.isUserResolvableError(isAvailable)){
-            Dialog msg =  api.getErrorDialog(this,isAvailable,0);
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog msg = api.getErrorDialog(this, isAvailable, 0);
             msg.show();
         } else {
             Toast.makeText(this, "Can't connect to play services :*(", Toast.LENGTH_LONG).show();
         }
         return false;
     }
-    /** Called when the user taps the login button **/
+
+    /**
+     * Called when the user taps the login button
+     **/
     public void goMap(View view) {
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
@@ -59,6 +70,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.bn_login:
+                SignIn();
+                break;
+        }
 
     }
 
@@ -66,5 +83,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+    private void SignIn()
+    {
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent,REQ_CODE);
+
+    }
+
+    private void startActivityForResult(Intent intent, int reqCode) {
+    }
+
+    private void handleResult(GoogleSignInResult result)
+    {
+        if(result.inSuccess())
+
+        {
+            GoogleSignInAccount account = result.getSignInAccount();
+            String email = account.getEmail();
+            
+
+
+        }
+    }
+    private void updateUI(boolean isLogin)
+    {
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==REQ_CODE)
+        {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
+    }
 }
+
 //
